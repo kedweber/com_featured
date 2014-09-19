1fe0058c0ea2e592b062fd767917132fda6c770a
Jasper van Rijbroek <jasper@jvar.nl>
2014-09-19 15:20:34 +0200

    Fixed the multilingual saving of featured.

--- packages/component/administrator/components/com_featured/databases/tables/nodes.php
+++ packages/component/administrator/components/com_featured/databases/tables/nodes.php
@@ -4,6 +4,34 @@ defined('KOOWA') or die('Protected resource');
 
 class ComFeaturedDatabaseTableNodes extends KDatabaseTableDefault
 {
+    public function __construct(KConfig $config)
+    {
+        parent::__construct($config);
+
+        // We will automagically set the name.
+        // This is done so the featurable works on two or more languages.
+        $lang_tag = JFactory::getLanguage()->getTag();
+        $parts = explode('-', $lang_tag);
+        $lang = $parts[0];
+        $base = 'featured_nodes';
+        $table = ($lang != 'en' ? $lang . '_' : '') . $base;
+
+        try
+        {
+            if($this->getDatabase()->getTableSchema($table))
+            {
+                // Do nothing, because the table exists.
+            }
+        }
+        catch(Exception $e)
+        {
+            // The table doesn't exist, so reset it.
+            $table = $base;
+        }
+
+        $this->_base = $table;
+    }
+
 	/**
 	 * @param KConfig $config
 	 */
@@ -13,7 +41,7 @@ class ComFeaturedDatabaseTableNodes extends KDatabaseTableDefault
 			'behaviors' => array(
 				'com://admin/moyo.database.behavior.creatable',
 				'modifiable',
-				'identifiable',
+				'identifiable'
 			)
 		));
 
