9d392d447d2ab7ce51487b957f80f3800dd3af93
jvdheijden <joep.van.der.heijden@moyoweb.nl>
2014-09-22 09:53:20 +0200

    Use translatable on frontend.

--- packages/component/components/com_featured/databases/tables/nodes.php
+++ packages/component/components/com_featured/databases/tables/nodes.php
@@ -4,29 +4,18 @@ defined('KOOWA') or die('Protected resource');
 
 class ComFeaturedDatabaseTableNodes extends KDatabaseTableDefault
 {
-    public function __construct(KConfig $config)
+
+    /**
+     * @param KConfig $config
+     */
+    protected function  _initialize(KConfig $config)
     {
-        parent::__construct($config);
+        $config->append(array(
+            'behaviors' => array(
+                'com://admin/translations.database.behavior.translatable'
+            )
+        ));
 
-        // We will automagically set the name.
-        // This is done so the featurable works on two or more languages.
-        $lang_tag = JFactory::getLanguage()->getTag();
-        $parts = explode('-', $lang_tag);
-        $lang = $parts[0];
-        $base = 'featured_nodes';
-        $table = ($lang != 'en' ? $lang . '_' : '') . $base;
-
-        try
-        {
-            $this->getDatabase()->getTableSchema($table);
-        }
-        catch(Exception $e)
-        {
-            // The table doesn't exist, so reset it.
-            $table = $base;
-        }
-
-        $this->_base = $table;
-        $this->_name = $table;
+        parent::_initialize($config);
     }
 }
